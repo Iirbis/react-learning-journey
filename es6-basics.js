baa76889d4fec82805dfe3a499da57af847a0d6b
Nia <nia@Nias-MacBook-Air.local>
2025-05-26 14:20:29 -0500

    Refactor arrow function challenges with clean syntax and naming

--- es6-basics.js
+++ es6-basics.js
@@ -11,3 +11,15 @@ let currentUser = "John Doe";
 // Task 3: Create a constant for the maximum number of visitors
 const BIRTHDAY = "18.04.1982";
 const age = someCode(BIRTHDAY);
+
+//Arrow Functions Challenges
+//Challenge 1: Create a function that returns the square of a number
+let squareNumber = (number) => number * number;
+//Challenge 2: Return a greetuing with a name
+let greeting =(name) => "Hello, " + name + "!";
+//Challenge 3: Return true if a number is even
+let isEven = (number) => number % 2 === 0 ? true : false;
+//Challenge 4: Add two numebrs and divide the rsult by 2
+let numbers =(num1, num2) => (num1 +num2)/2;
+//Challenge 5: Return the last character of a string
+let lastCharacter = (word) => word[word.length - 1];
