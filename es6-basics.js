3cbbfc1030c7c5fc9193bbc261b38ab24af63d35
Nia <nia@Nias-MacBook-Air.local>
2025-05-26 13:31:19 -0500

    Complete JavaScript.info variable tasks (Day 1)

---
+++ es6-basics.js
@@ -0,0 +1,13 @@
+// --- JavaScript.info: Variables Tasks ---
+// Task 1: Declare two variables: admin and name
+let name =  "John";
+let admin = name;
+alert(admin); // John
+
+// Task 2: Giving the variable name, create a variable with the name of the user
+let ourPlanetName = "Earth";
+let currentUser = "John Doe";
+
+// Task 3: Create a constant for the maximum number of visitors
+const BIRTHDAY = "18.04.1982";
+const age = someCode(BIRTHDAY);
