b87d4bfb6592cb9ac49b413596aca8ad5c97c6e7
Nia <nia@Nias-MacBook-Air.local>
2025-05-28 18:33:29 -0500

    Complete Day 3 destructuring, spread, and rest challenges

--- es6-basics.js
+++ es6-basics.js
@@ -42,3 +42,49 @@ const adults = ages.filter(age => age >= 18);
 //Challenge 4: “The Grand Total”
 const prices = [10.99, 5.49, 20.00, 3.50];
 const totalCost = prices.reduce((total, price) => total + price, 0);
+
+//--DESTRUCTURING--
+//Challenge 1: "Loot the Array"
+const bestSellers = [
+  "Fourth Wing",
+  "From Lukov with Love",
+  "A Court of Thorns and Roses",
+  "The Seven Husbands of Evelyn Hugo",
+  "Bride"
+];
+const [first,second,...remaining] = bestSellers; 
+
+//Challenge 2: "Hero Stats"
+const character = {
+  name: "Spark",
+  class: "Rogue",
+  level: 21,
+  skills: ["Stealth", "Backstab", "Evade"],
+  origin: "Shadowreach"
+};
+const {name, class: role, level, skills} = character;
+
+//Challenge 3: "Greeter Function"
+const user = {
+  username: "Spark",
+  title: "Shadow Dancer",
+  level: 42
+};
+
+function greetUser({username, title, level}){
+    console.log(`Welcome, ${title} ${username}! You are level ${level}.`);
+}
+greetUser(user);
+
+//---Spread/Rest---
+//Challenge 1: Cart Expansion
+const cart = ["Apples", "Bananas"];
+const newItems = ["Oranges", "Grapes"];
+const updatedCart = [...cart, ...newItems];
+
+//Challenge 2: Name Drop
+function introduce(leader, ...followers){
+    console.log(`${leader} is leading the team. ${followers.join(", ")} are following.`);
+}
+
+introduce("Ashy", "Spark", "Mischa", "Lucy");
